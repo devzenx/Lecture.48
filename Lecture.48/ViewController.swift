5bb82f752f22a6546e7e7c9eee01821c8ed6a94f
stephane <contact@depoilly.com>
2016-03-18 10:18:16 +0100

    Create player and add random enemies

--- Lecture.48/ViewController.swift
+++ Lecture.48/ViewController.swift
@@ -23,9 +23,30 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var chestBtn: UIButton!
     
+    
+    var player: Player!
+    var enemy: Enemy!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        player = Player(name: "Dirty Laundry21", hp: 110,attackPwr:20)
+        
+        playerHpLbl.text = "\(player.hp) HP"
+        
+        
+    }
+    
+    func generateRandomEnemy(){
+        let rand = Int(arc4random_uniform(2))
+        
+        if rand == 0 {
+            enemy = Kimara(startingHp: 50, attackPwr: 12)
+        } else {
+            enemy = DevilWizard(startingHp: 60, attackPwr: 15)
+        }
     }
 
     @IBAction func onChestTapped(sender: AnyObject) {
